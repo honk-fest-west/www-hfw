039663eaef270e21059484a0ef1b81c0f3a2b0d6
Dwight VanTuyl <dvantuyl@gmail.com>
2021-08-07 16:17:58 -0700

    Home | Info and Newsletter blocks

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,27 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   mode: 'jit',
   purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}'],
   theme: {
     colors: {
-      'background-color': '#a0cfc5',
-      'nav-color': '#3f746c',
+      'main-color': {
+        DEFAULT: '#5CAD9C',
+        50: '#F5FAF9',
+        100: '#E4F1EE',
+        200: '#C2E0DA',
+        300: '#A0CFC5',
+        400: '#7EBEB1',
+        500: '#5CAD9C',
+        600: '#488E80',
+        700: '#376C61',
+        800: '#254B43',
+        900: '#142924',
+      },
+      'title-primary-color': '#489ca7',
+      'title-secondary-color': '#6c757d',
+      'link-color': '#dc5849',
+      white: colors.white,
     },
     extend: {
       backgroundImage: (theme) => ({
@@ -13,6 +30,8 @@ module.exports = {
       }),
       fontFamily: {
         title: ['Josefin Sans', 'sans-serif'],
+        emphasis: ['Teko', 'sans-serif'],
+        base: ['Poppins', 'sans-serif'],
       },
     },
   },
