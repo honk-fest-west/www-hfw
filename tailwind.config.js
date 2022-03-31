597517cadb3ef893a1e4b3518fd27c160258cbe2
Dwight VanTuyl <dvantuyl@gmail.com>
2022-03-30 21:39:30 -0700

    2022 Rebrand

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,30 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ['./src/**/*.{html,astro,js,jsx,ts,tsx,vue,svelte}'],
   theme: {
     extend: {
       colors: {
+        gray: colors.stone,
         'main-color': {
           DEFAULT: 'var(--main-color)',
-          50: '#8494C6',
-          100: '#7385BE',
-          200: '#5067AE',
-          300: '#40528B',
-          400: '#303E68',
+          50: '#FFFFFF',
+          100: '#FFFFFF',
+          200: '#F8F2DD',
+          300: '#F2E5BB',
+          400: '#EBD899',
           500: 'var(--main-color)',
-          600: '#101422',
-          700: '#000000',
-          800: '#000000',
-          900: '#000000',
+          600: '#DBB948',
+          700: '#C4A027',
+          800: '#957A1E',
+          900: '#665314',
         },
-        'title-primary-color': '#8494C6',
-        'accent-color': '#F57F45',
+        'title-primary-color': '#F1AA00',
+        'accent-color': '#962C2B',
         'content-container': '#4D546A',
       },
       backgroundImage: (theme) => ({
-        site: 'url(/assets/images/birds-background.png)',
+        site: 'url(https://ik.imagekit.io/thegoldendino/hfw/shatter-background_1__yeFkQzR5s.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1648439951103)',
       }),
       fontFamily: {
         title: ['Josefin Sans', 'sans-serif'],
