3d02b31fc5b1a8e108535fafdc5def91c8846c79
Dwight VanTuyl <dvantuyl@gmail.com>
2022-05-30 14:28:47 -0700

    Festival | Fix schedule styling

--- src/components/festival/ScheduleTable.svelte
+++ src/components/festival/ScheduleTable.svelte
@@ -24,7 +24,9 @@
 						<tr>
 							<th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 lg:pl-8 w-28"></th>
 							{#each day.stages as stageName}
-								<th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 w-1/4"><p>{stageName}</p></th>
+								<th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 w-1/4">
+									<p class="text-ellipsis overflow-clip">{stageName}</p>
+								</th>
 							{/each}
 						</tr>
 					</thead>
@@ -33,9 +35,9 @@
 							<tr>
 								<td class="whitespace-nowrap py-4 pl-4 pr-3 font-medium text-gray-900 sm:pl-6 lg:pl-8"><p>{shortTime(timeSlot.t)}</p></td>
 								{#each timeSlot.b as bandkey}
-									<td class="whitespace-nowrap px-3 py-4 text-ellipsis w-1/4 overflow-clip text-sm text-main-color-600">
+									<td class="px-3 py-4 w-1/4">
 										{#if bandkey}
-											<a href={bands[bandkey].url}>{bands[bandkey].name}</a>
+											<p class="text-ellipsis overflow-clip text-main-color-600 text-sm md:text-lg pr-2"><a href={bands[bandkey].url}>{bands[bandkey].name}</a></p>
 										{/if}
 									</td>
 								{/each}
