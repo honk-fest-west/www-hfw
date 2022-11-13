dcf131fd44f6d1ac06848b68c5c45df23ed1a066
Dwight VanTuyl <dvantuyl@gmail.com>
2022-11-12 21:42:21 -0800

    Portal | Fix portal with custom tailwindcss config

---
+++ src/styles/tailwind.portal.config.cjs
@@ -0,0 +1,6 @@
+const colors = require('tailwindcss/colors');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,astro,js,jsx,ts,tsx,vue,svelte}'],
+};
