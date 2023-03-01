0b620ad683daf40a1a13c2e2734eb07d53c60e0d
Dwight VanTuyl <dvantuyl@gmail.com>
2023-02-28 17:36:21 -0800

    Remove pages from the sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,30 +2,32 @@ import { defineConfig } from 'astro/config';
 import yaml from '@rollup/plugin-yaml';
 import { ViteToml } from 'vite-plugin-toml';
 
-// https://astro.build/config
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 import partytown from '@astrojs/partytown';
 
-// https://astro.build/config
 import image from '@astrojs/image';
 
-// https://astro.build/config
 import svelte from '@astrojs/svelte';
 
-// https://astro.build/config
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
+const sitemap_filter_urls = [
+  'https://honkfestwest.org/donate/thankyou/',
+  'https://honkfestwest.org/portal/',
+  'https://honkfestwest.org/volunteer/committee-form-success/',
+  'https://honkfestwest.org/volunteer/committee-form/',
+  'https://honkfestwest.org/volunteer/housing-form/',
+  'https://honkfestwest.org/volunteer/new-volunteer-form-success/',
+  'https://honkfestwest.org/volunteer/new-volunteer-form/',
+];
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://honkfestwest.org',
   integrations: [
     tailwind({ config: { applyBaseStyles: false } }),
     svelte(),
-    sitemap(),
+    sitemap({ filter: (url) => !sitemap_filter_urls.includes(url) }),
     image(),
   ],
   vite: {
