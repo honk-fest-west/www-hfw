75dda3913c6de9b22623cea622e203b1b42676b1
Dwight VanTuyl <dvantuyl@gmail.com>
2023-05-03 19:16:14 -0700

    Festival App | Update Styling

--- src/components/festival/DaySelection.svelte
+++ src/components/festival/DaySelection.svelte
@@ -28,25 +28,27 @@
 </script>
 
 <div class="btn-group flex justify-around variant-filled rounded-none">
-  <a href="/" class="w-full">
+  <a href="/" class="flex-0">
     <img
       src={logoImg.src}
       width={logoImg.width}
       height={logoImg.height}
       alt="back to HONK!"
-      class="max-w-[9rem] mx-auto"
+      class="w-full max-w-[10rem] mx-auto"
     />
   </a>
   {#each days as day, idx}
     <button
       type="button"
-      class={`w-full ${selectedClass(
+      class={`w-full flex-1 ${selectedClass(
         idx
       )} flex flex-col items-center justify-center`}
       on:click={() => selectDayIdx(idx)}
     >
-      <span class="text-2xl capitalize block">{formatDay(day.date)}</span>
-      <span class="text-sm -ml-2 block">{formatDate(day.date)}</span>
+      <span class="text-2xl sm:text-3xl capitalize block"
+        >{formatDay(day.date)}</span
+      >
+      <span class="text-sm sm:text-lg block">{formatDate(day.date)}</span>
     </button>
   {/each}
 </div>
