d96867da0f399945ef325be529b8ffa636e91dec
Dwight VanTuyl <dvantuyl@gmail.com>
2023-05-05 12:56:26 -0700

    Festival App | Allow title to be actionable

--- src/components/festival/FestivalApp.svelte
+++ src/components/festival/FestivalApp.svelte
@@ -121,10 +121,14 @@
       />
     {/key}
     <div
-      class="flex justify-end px-3 py-2 gap-2 bg-primary-500 text-on-surface-token font-bold items-center"
+      class="flex justify-end px-3 py-2 gap-2 bg-primary-500 text-primary-50 font-bold items-center"
     >
       {#if $state.value === 'viewingMap'}
-        <h2 class="text-right">{selectedDay.location}</h2>
+        <h2 class="text-right">
+          <a class="unstyled" href={selectedDay.mapUrl}
+            ><span class="">{selectedDay.location}</span></a
+          >
+        </h2>
         <a
           href={selectedDay.mapUrl}
           class="btn-icon bg-surface-600 p-2 mx-3 rounded-xl flex-0 border-2 border-surface-400 text-surface-50"
@@ -135,7 +139,11 @@
         <button type="button" class="btn btn-icon p-2 flex-0" on:click={viewMap}
           ><IoMdArrowRoundBack /></button
         >
-        <h2 class="flex-1 text-right">{selectedStage.name}</h2>
+        <h2 class="flex-1 text-right">
+          <a class="unstyled" href={selectedStage.mapUrl}
+            >{selectedStage.name}</a
+          >
+        </h2>
         <a
           href={selectedStage.mapUrl}
           class="btn-icon bg-surface-600 p-2 mx-3 rounded-xl flex-0 border-2 border-surface-400 text-surface-50"
@@ -148,7 +156,9 @@
           class="btn btn-icon p-2 flex-0"
           on:click={viewStage}><IoMdArrowRoundBack /></button
         >
-        <h2 class="flex-1 text-right">{selectedBand.name}</h2>
+        <h2 class="flex-1 text-right">
+          <a class="unstyled" href={selectedBand.url}>{selectedBand.name}</a>
+        </h2>
         <a
           class="btn-icon bg-surface-600 p-2 mx-3 rounded-xl flex-0 border-2 border-surface-400 text-surface-50"
           href={selectedBand.url}
