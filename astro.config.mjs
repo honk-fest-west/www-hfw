5386ba7639a2bb41d96d29e17eb234bd8ef3f2da
Dwight Van Tuyl <dvantuyl@gmail.com>
2024-05-13 17:33:22 +0000

    update supporter images

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,13 +11,17 @@ const sitemap_filter_urls = ["https://honkfestwest.org/donate/thankyou/", "https
 // https://astro.build/config
 export default defineConfig({
   site: "https://honkfestwest.org/",
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), sitemap({
-    filter: url => !sitemap_filter_urls.includes(url)
-  }), svelte()],
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false
+      }
+    }), 
+    sitemap({
+      filter: url => !sitemap_filter_urls.includes(url)
+    }), 
+    svelte()
+  ],
   vite: {
     plugins: [yaml(), ViteToml()]
   },
